fe0e493a6896961aa3f90f9430a0df8837eaa832
developerTamanna <astamanna57@gmail.com>
2025-04-01 00:01:03 +0600

    add to cart

--- src/components/bottle/bottle.jsx
+++ src/components/bottle/bottle.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
 
-const Bottle = ({bottle}) => {
-    console.log(bottle)
+const Bottle = ({bottle, handleAddToCart}) => {
+    const {img, name, price, stock} =bottle
+    // console.log(bottle)
     return (
-        <div className='border-2 border-green-500 p-5 rounded-2xl'>
-            <img src={bottle.img} alt=""></img>
-            <p className='text-xm font-bold'>name: {bottle.name}</p>
-            <small className='font-bold text-gray-500'> price: {bottle.price}</small>
+        <div className='border-2 border-green-500 p-5 rounded-2xl h-[350px] flex flex-col'>
+            <img className='w-full h-40 object-cover rounded-lg' src={img} alt=""></img>
+            <p className='text-sm font-bold border-2 border-pink-100 text-center rounded-lg bg-pink-50 mt-2 p-1'>name: {name}</p>
+            <h6 className='font-bold text-gray-500 mt-auto'> price: {price}</h6>
+            <small className='font-bold text-gray-400'> {stock} remaining</small>
+            <button onClick={()=>handleAddToCart(bottle)} className='mt-4'>Buy Now</button>
         </div>
     );
 };
--- src/components/bottles/bottles.jsx
+++ src/components/bottles/bottles.jsx
@@ -1,19 +1,40 @@
-import React, { use } from 'react';
+import React, { use, useState } from 'react';
 import Bottle from '../bottle/bottle';
 const Bottles = ({bottlesPromise}) => {
+ 
+
+    // cart++
+    const [cart, setCart] = useState([]);
+
+
+   const handleAddToCart =(bottle)=>{
+    // console.log('button clicked', bottle)
+    const newCart = [...cart, bottle]
+    setCart(newCart)
+   }
+
+
+
     const bottles = use(bottlesPromise)
-    console.log(bottles)
+
+    // console.log(bottles)
     return (
         <div  >
            <div className='border-2 p-5 mt-20 rounded-xl w-40'>
            <h3 className='text-2xl font-semibold text-blue-800'>Bottles:{bottles.length}</h3>
            </div>
+           <div className='border-2 p-5 mt-20 rounded-xl w-40'>
+           <h3 className='text-xl font-semibold text-blue-800'>Added to cart:{cart.length}</h3>
+           </div>
 
           <div className='grid grid-cols-3 gap-5 mt-20'>
           {
                 bottles.map(bottle=>
-                <Bottle bottle={bottle}
-                 key={bottle.id}>
+                <Bottle bottle=
+                {bottle}
+                 key={bottle.id}
+                 handleAddToCart={handleAddToCart}
+                 >
 
                 </Bottle>)
              }
