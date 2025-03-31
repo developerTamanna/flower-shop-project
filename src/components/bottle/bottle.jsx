4ff0edc655f47902589eaff9b694bc9f3203ea11
developerTamanna <astamanna57@gmail.com>
2025-03-31 22:18:45 +0600

    footer

--- src/components/bottle/bottle.jsx
+++ src/components/bottle/bottle.jsx
@@ -5,6 +5,8 @@ const Bottle = ({bottle}) => {
     return (
         <div className='border-2 border-green-500 p-5 rounded-2xl'>
             <img src={bottle.img} alt=""></img>
+            <p className='text-xm font-bold'>name: {bottle.name}</p>
+            <small className='font-bold text-gray-500'> price: {bottle.price}</small>
         </div>
     );
 };
